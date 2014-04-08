1ac3a03ee71c377019f45c81012fbd91b1283fe9
Eloy Espinaco <eloyesp@gmail.com>
2014-04-08 05:33:33 -0300

    Primer examen decente.

--- examen_enteros_2.rb
+++ examen_enteros_2.rb
@@ -1,8 +1,31 @@
 require 'erb'
 require 'pry'
 
-def ejercicio_1
-  "$#{ rand(10) - 5 } + #{ rand(10) - 5 } =$"
+def formatear numero
+  numero < 0 ? "(#{ numero})" : numero
+end
+
+def numero_entero cifras = 1
+  rango = cifras * 10
+  numero = rand(rango * 2) - rango
+  formatear numero
+end
+
+def sumas_y_restas_con_dos_cifras
+  "$#{ formatear rand(10..30) } - #{ formatear rand(10..90) } - #{ formatear rand(10..90) } + #{ formatear rand(10..90) } =$"
+end
+
+def sumas_con_tres_cifras
+  "$#{ formatear rand(100..600) } + #{ formatear rand(-1000..-90) } + #{ formatear rand(-1000..-90) } =$"
+end
+
+def resta_de_tres_cifras
+  n = rand(1000..6000)
+  "$#{ formatear n } - #{ n + rand(3000) } =$"
+end
+
+def sumas_y_restas_de_una_cifra
+  "$#{ formatear rand(2..10) } + #{ formatear rand(2..10) } - #{ formatear rand(2..10) } =$"
 end
 
 def build
