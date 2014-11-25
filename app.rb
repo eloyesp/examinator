1f359c74912adb0257d53e561d0c93e536d94e7d
Eloy Espinaco <eloyesp@gmail.com>
2014-11-25 17:40:47 -0300

    Parse erb.

--- app.rb
+++ app.rb
@@ -1,18 +1,33 @@
+require 'erb'
+
 class App
   def call env
-    ['200', {'Content-Type' => 'text/html'}, contents(env['REQUEST_PATH'])]
+    [ '200',
+      {'Content-Type' => 'text/html'},
+      build(env['REQUEST_PATH'])
+    ]
   end
 
-  def contents file
-    command = <<CMD
-| pandoc \
-  --to html5 \
-  --from markdown \
-  --standalone \
-  --css "/css/examinator.css" \
-  examenes#{ file }
-CMD
-    File.readlines(command) #examenes#{ file }")
+  def build file
+    raise 'Template format is wrong' unless file.match(/\.erb$/)
+    template = File.read "examenes#{ file }"
+    erb = ERB.new template, nil, '<>'
+    result = erb.result
+    process_with_pandoc result
+  end
+
+  def process_with_pandoc text
+    command = "| pandoc " \
+              " --to html5 " \
+              " --from markdown " \
+              " --standalone " \
+              " --css '/css/examinator.css' " \
+              " - "
+    open command, 'r+' do |pandoc|
+      pandoc.puts text
+      pandoc.close_write
+      pandoc.readlines
+    end
   end
 
   def footer env
