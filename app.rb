0f10dac09d7493cb137d96df7c3382936bbf93c1
Eloy Espinaco <eloyesp@gmail.com>
2014-11-25 16:56:36 -0300

    Build the file online.

--- app.rb
+++ app.rb
@@ -1,10 +1,29 @@
 class App
   def call env
-    ['200', {'Content-Type' => 'text/html'}, contents]
+    ['200', {'Content-Type' => 'text/html'}, contents(env['REQUEST_PATH'])]
   end
 
-  def contents
-    `./process_markdown`
-    File.readlines('examenes/primero_diciembre-marzo.html')
+  def contents file
+    command = <<CMD
+| pandoc \
+  --to html5 \
+  --from markdown \
+  --standalone \
+  --css "/css/examinator.css" \
+  examenes#{ file }
+CMD
+    File.readlines(command) #examenes#{ file }")
+  end
+
+  def footer env
+    ['<footer>',
+     env.map do |key, value|
+       <<-HTML.gsub(/^\s+\|/,'')
+       | <dt>#{ key }</dt>
+       | <dd>#{ value }</dd>
+       HTML
+     end.join("\n"),
+     '</footer>'
+    ].join("\n")
   end
 end
--- config.ru
+++ config.ru
@@ -2,5 +2,6 @@
 require_relative 'app'
 
 use Rack::Reloader
-
-run App.new
+use Rack::Static, :urls => ["/css", "/images"]
+run Rack::Directory.new 'examenes', App.new
+use Rack::ShowStatus
